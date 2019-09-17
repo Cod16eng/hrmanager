70f72eaeacfc550d65de9ce21a7614b450170026
Codrin Popescu <codrin@ascheri.co.uk>
2019-09-17 08:46:07 +0100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.delete.all
+Department.delete.all
+User.delete.all
+Company.create!(name: "Test")
+Department.create!(name: "Test", company_id: 1)
+User.create!(first_name: "Codrin", last_name: "Popescu", email: "codrin@ascheri.co.uk", 
+			 password: "Codrin31", password_confirmation: "Codrin31", admin: true, 
+			 company_id: 1, department_id: 1)
